4e80ac317e0f9d934ccf1eb67166c4f4d69a83ba
ricardo.urbano <ricardo.urbano@cejam.org.br>
2025-06-16 23:11:07 -0300

    inclusao de paths para src e bsca da imagem em assets

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -7,7 +7,7 @@ import { styles } from "./styles"
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require('../../assets/logo.png')} style={styles.logo} />
+      <Image source={require('@/assets/logo.png')} style={styles.logo} />
       <Text style={styles.text}>Open up App.tsx to start woraaaking on your app! teste w</Text>
       <StatusBar style="auto" />
     </View>
